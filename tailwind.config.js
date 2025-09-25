69aa7c1d8e04f31d332be21d8af3371ec546be2c
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-09-25 15:06:39 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [
+
+  ],
+};
